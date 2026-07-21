11438c4a5bc7c9773fb3da8c08641e71f0603c6c
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-21 02:01:35 -0300

    fix: support disabled country filter

--- src/modules/otFallidasCT/components/elementos/Pais.vue
+++ src/modules/otFallidasCT/components/elementos/Pais.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="fm-field fm-field--span-3">
+  <div
+    class="fm-field fm-field--span-3 otf-filter-element"
+    :class="{ 'otf-filter-element--disabled': disabled }"
+  >
     <label for="pais">País</label>
     <Select
       id="pais"
@@ -8,6 +11,7 @@
       optionLabel="valor"
       placeholder="Seleccione"
       showClear
+      :disabled="disabled"
     />
   </div>
 </template>
@@ -15,6 +19,10 @@
 <script setup>
 import { ref, computed } from 'vue'
 import { useFallidasCtStore } from '../../store/CtFallidaStore'
+
+defineProps({
+  disabled: { type: Boolean, default: false }
+})
 
 const store = useFallidasCtStore()
 const paises = ref([
