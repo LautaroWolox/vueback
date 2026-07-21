98bd6023b37d2310a94b58c868846f5878933bb6
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-21 01:04:42 -0300

    feat: add global route loading state

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Suspense>
+      <component :is="Component" />
+
+      <template #fallback>
+        <FmTypingLoader fullscreen title="Cargando" message="Cargando pantalla" />
+      </template>
+    </Suspense>
+  </RouterView>
 </template>
 
 <script setup>
 </script>
-
-<style scoped>
-</style>
