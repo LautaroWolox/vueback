7b1385db5ee476b1c35d1bd9b4feb4a911e1229e
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-21 00:58:56 -0300

    feat: add Field Manager PrimeVue passthrough

---
+++ src/components/shared/primePassThrough.js
@@ -0,0 +1,76 @@
+export const fmPrimePassThrough = {
+  button: {
+    root: { class: 'fm-pt-button fm-action-button' },
+    label: { class: 'fm-pt-button-label' },
+    icon: { class: 'fm-pt-button-icon' }
+  },
+  inputtext: {
+    root: { class: 'fm-pt-inputtext fm-input' }
+  },
+  textarea: {
+    root: { class: 'fm-pt-textarea fm-input' }
+  },
+  select: {
+    root: { class: 'fm-pt-select fm-select' },
+    label: { class: 'fm-pt-select-label' },
+    dropdown: { class: 'fm-pt-select-dropdown' },
+    panel: { class: 'fm-pt-select-panel' },
+    list: { class: 'fm-pt-select-list' },
+    option: { class: 'fm-pt-select-option' }
+  },
+  multiselect: {
+    root: { class: 'fm-pt-multiselect fm-select' },
+    labelContainer: { class: 'fm-pt-multiselect-label' },
+    dropdown: { class: 'fm-pt-multiselect-dropdown' },
+    panel: { class: 'fm-pt-multiselect-panel' },
+    list: { class: 'fm-pt-multiselect-list' },
+    option: { class: 'fm-pt-multiselect-option' },
+    header: { class: 'fm-pt-multiselect-header' }
+  },
+  accordion: {
+    root: { class: 'fm-pt-accordion fm-accordion' }
+  },
+  accordionpanel: {
+    root: { class: 'fm-pt-accordion-panel' }
+  },
+  accordionheader: {
+    root: { class: 'fm-pt-accordion-header' },
+    toggleicon: { class: 'fm-pt-accordion-toggle-icon' }
+  },
+  accordioncontent: {
+    content: { class: 'fm-pt-accordion-content' }
+  },
+  datatable: {
+    root: { class: 'fm-pt-datatable fm-pass-grid' },
+    table: { class: 'fm-pt-datatable-table' },
+    header: { class: 'fm-pt-datatable-header' },
+    footer: { class: 'fm-pt-datatable-footer' },
+    wrapper: { class: 'fm-pt-datatable-wrapper' },
+    thead: { class: 'fm-pt-datatable-thead' },
+    tbody: { class: 'fm-pt-datatable-tbody' },
+    row: { class: 'fm-pt-datatable-row' },
+    bodyRow: { class: 'fm-pt-datatable-body-row' },
+    columnHeaderContent: { class: 'fm-pt-column-header' },
+    paginator: { class: 'fm-pt-paginator' }
+  },
+  checkbox: {
+    root: { class: 'fm-pt-checkbox' },
+    box: { class: 'fm-pt-checkbox-box' },
+    icon: { class: 'fm-pt-checkbox-icon' }
+  },
+  dialog: {
+    root: { class: 'fm-pt-dialog' },
+    header: { class: 'fm-pt-dialog-header' },
+    content: { class: 'fm-pt-dialog-content' },
+    footer: { class: 'fm-pt-dialog-footer' }
+  },
+  datepicker: {
+    root: { class: 'fm-pt-datepicker fm-date-picker' },
+    input: { class: 'fm-pt-datepicker-input fm-date-button' },
+    panel: { class: 'fm-pt-datepicker-panel fm-calendar' },
+    header: { class: 'fm-pt-datepicker-header fm-calendar-head' },
+    day: { class: 'fm-pt-datepicker-day' }
+  }
+}
+
+export default fmPrimePassThrough
