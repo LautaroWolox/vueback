8aa582b3286b8348d02eb89fc9296c31ad938936
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-21 02:00:04 -0300

    fix: support disabled start date filter

--- src/modules/otFallidasCT/components/elementos/FechaDesde.vue
+++ src/modules/otFallidasCT/components/elementos/FechaDesde.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="fm-field fm-field--span-3">
+  <div
+    class="fm-field fm-field--span-3 otf-filter-element"
+    :class="{ 'otf-filter-element--disabled': disabled }"
+  >
     <label for="fecha-desde">Fecha desde</label>
     <CtDatePicker
       input-id="fecha-desde"
       v-model="fechaDesde"
       placeholder="Seleccionar fecha"
+      :disabled="disabled"
     />
   </div>
 </template>
@@ -14,9 +18,13 @@ import { computed } from 'vue'
 import CtDatePicker from '../CtDatePicker.vue'
 import { useFallidasCtStore } from '../../store/CtFallidaStore'
 
+defineProps({
+  disabled: { type: Boolean, default: false }
+})
+
 const store = useFallidasCtStore()
 const fechaDesde = computed({
   get: () => store.filters.fechaCierreOTDesde,
-  set: (value) => store.setFilter('fechaCierreOTDesde', value || '')
+  set: (value) => store.setFilter('fechaCierreOTDesde', value || null)
 })
 </script>
