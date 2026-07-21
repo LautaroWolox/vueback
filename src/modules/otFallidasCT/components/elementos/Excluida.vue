2b6150e1e7058e99843734b1eb34ff3fa98b8522
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-21 01:26:39 -0300

    style: apply Field Manager exclusion filter

--- src/modules/otFallidasCT/components/elementos/Excluida.vue
+++ src/modules/otFallidasCT/components/elementos/Excluida.vue
@@ -1,31 +1,30 @@
 <template>
-    <div class="flex flex-column px-2">
-        <label for="excl">Excluida</label>
-        <Select id="excl" v-model="exc" :options="excluida" optionLabel="valor"class="w-full md:w-56" />
-    </div>
+  <div class="fm-field fm-field--span-3">
+    <label for="excl">Excluida</label>
+    <Select
+      id="excl"
+      v-model="exc"
+      :options="excluida"
+      optionLabel="valor"
+      placeholder="Seleccione"
+      showClear
+    />
+  </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed } from 'vue'
 import { useFallidasCtStore } from '../../store/CtFallidaStore'
 
 const store = useFallidasCtStore()
-
-const excluida = ref ([
-    {'id': '', 'valor': ''},
-    {'id': 'S', 'valor': 'SI'},
-    {'id': 'N', 'valor': 'NO'},
+const excluida = ref([
+  { id: '', valor: '' },
+  { id: 'S', valor: 'SI' },
+  { id: 'N', valor: 'NO' }
 ])
 
 const exc = computed({
-  get: () =>
-    excluida.value.find(p => p.valor === store.filters.excluida) ?? exc.value[0],
-
-  set: (value) =>
-    store.setFilter('excluida', value?.id ?? '')
+  get: () => excluida.value.find((item) => item.id === store.filters.excluida) ?? excluida.value[0],
+  set: (value) => store.setFilter('excluida', value?.id ?? '')
 })
-
 </script>
-
-<style scoped>
-</style>
