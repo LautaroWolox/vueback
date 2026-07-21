a99ba5febdfedfa7ef0798ba1193943c817c66cf
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-21 01:27:39 -0300

    style: apply Field Manager layout to failed OTs

--- src/modules/otFallidasCT/OtFallidasCT.vue
+++ src/modules/otFallidasCT/OtFallidasCT.vue
@@ -1,36 +1,33 @@
 <template>
-    <LoadingOverlay :loading="store.loading" />
-    <div class="mx-2">
-        <Accordion v-model:value=store.activeTab multiple>
-            <AccordionPanel value="0">
-                <AccordionHeader>
-                    FILTROS DE BÚSQUEDA
-                </AccordionHeader>
-                <AccordionContent>
-                    <Filtros />
-                </AccordionContent>
-            </AccordionPanel>
-            <AccordionPanel value="1">
-                <AccordionHeader>
-                   OTS FALLIDAS REPROCESO
-                </AccordionHeader>
-                <AccordionContent>
-                    <Table/>
-                </AccordionContent>
-            </AccordionPanel>
-        </Accordion>
-    </div>
+  <div class="fm-screen fm-screen--pad otf-page">
+    <Accordion v-model:value="store.activeTab" multiple class="fm-accordion">
+      <AccordionPanel value="0">
+        <AccordionHeader>FILTROS DE BÚSQUEDA</AccordionHeader>
+        <AccordionContent>
+          <Filtros />
+        </AccordionContent>
+      </AccordionPanel>
+
+      <AccordionPanel value="1">
+        <AccordionHeader>OTS FALLIDAS REPROCESO</AccordionHeader>
+        <AccordionContent>
+          <Table />
+        </AccordionContent>
+      </AccordionPanel>
+    </Accordion>
+  </div>
 </template>
 
 <script setup>
 import { useFallidasCtStore } from './store/CtFallidaStore'
-import Table from './components/Table.vue';
-import Filtros from './components/Filtros.vue';
-import LoadingOverlay from '../shared/components/LoadingOverlay.vue';
+import Table from './components/Table.vue'
+import Filtros from './components/Filtros.vue'
 
 const store = useFallidasCtStore()
-//const active = store.activeTab
-
 </script>
 
-<style scoped></style>
+<style scoped>
+.otf-page {
+  min-height: calc(100vh - 82px);
+}
+</style>
