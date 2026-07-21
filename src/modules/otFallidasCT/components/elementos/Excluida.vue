0d3b9b3e07daed5d25a6e9f72c02f90af12f8e23
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-21 03:18:43 -0300

    style: restore approved excluded filter span

--- src/modules/otFallidasCT/components/elementos/Excluida.vue
+++ src/modules/otFallidasCT/components/elementos/Excluida.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fm-field fm-field--span-3 otf-filter-element"
+    class="fm-field fm-field--span-2 otf-filter-element otf-filter-element--excluida"
     :class="{ 'otf-filter-element--disabled': disabled }"
   >
     <label for="excl">Excluida</label>
