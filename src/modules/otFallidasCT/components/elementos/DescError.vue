34f61093ab0edd4f07b36fc77e1a841d2e202411
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-21 01:26:21 -0300

    style: apply Field Manager error filter

--- src/modules/otFallidasCT/components/elementos/DescError.vue
+++ src/modules/otFallidasCT/components/elementos/DescError.vue
@@ -1,24 +1,18 @@
 <template>
-    <div class="flex flex-column px-2">
-        <label for="desc">Descripción Error</label>
-        <InputText type="text" id="error" v-model="desc" />
-    </div>
+  <div class="fm-field fm-field--span-3">
+    <label for="error">Descripción del error</label>
+    <InputText id="error" v-model="desc" type="text" autocomplete="off" />
+  </div>
 </template>
 
-
 <script setup>
-import { computed } from 'vue';
-import InputText from 'primevue/inputtext';
+import { computed } from 'vue'
+import InputText from 'primevue/inputtext'
 import { useFallidasCtStore } from '../../store/CtFallidaStore'
 
 const store = useFallidasCtStore()
 const desc = computed({
   get: () => store.filters.descripcionError,
-  set: (desc) =>
-    store.setFilter('descripcionError', desc? desc : '') 
+  set: (value) => store.setFilter('descripcionError', value || '')
 })
-
 </script>
-
-<style scoped>
-</style>
