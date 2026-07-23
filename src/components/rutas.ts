ead1d24a8d2702d35da7b65ed14c1d0c3e9c1d38
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-23 14:05:40 -0300

    fix: rename jobtype CMO menu to CMO activity

--- src/components/rutas.ts
+++ src/components/rutas.ts
@@ -139,14 +139,9 @@ export const getRutas = (rutas:any) => {
                             visible: hasMenu('JOCO')
                         },
                         {
-                            label: 'Configuración Jobtype-CMO',
+                            label: 'Configuración CMO-Actividad',
                             command: () => { router.push({ name: 'JOCM' }); },
                             visible: hasMenu('JOCM') || hasMenu('JOCO')
-                        },
-                        {
-                            label: 'Configuración CMO-Actividad',
-                            command: () => { router.push({ name: 'CMOA' }); },
-                            visible: hasMenu('CMOA')
                         }
                     ]
                 },
