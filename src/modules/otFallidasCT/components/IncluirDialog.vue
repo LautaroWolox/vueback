12ee588890ee0850a86191fef557623469824df0
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-23 11:34:03 -0300

    fix: update OT inclusion dialog labels

--- src/modules/otFallidasCT/components/IncluirDialog.vue
+++ src/modules/otFallidasCT/components/IncluirDialog.vue
@@ -8,6 +8,10 @@
     @update:visible="$emit('update:visibleInc', $event)"
   >
     <div class="otf-include-form">
+      <p class="otf-include-confirmation">
+        ¿Confirma que desea incluir OT seleccionada?
+      </p>
+
       <div class="fm-field otf-include-motivo-field">
         <label for="motivo-inclusion">Motivo</label>
         <FmCompactSelect
@@ -31,9 +35,9 @@
       </div>
 
       <div class="fm-field otf-include-comment-field">
-        <label for="comentario-inclusion">Comentario</label>
+        <label for="nota-inclusion">Nota</label>
         <textarea
-          id="comentario-inclusion"
+          id="nota-inclusion"
           v-model="comentario"
           class="otf-include-comment"
           rows="2"
@@ -103,6 +107,14 @@ onMounted(() => commonCT.setMotivosExcInc())
   display: flex;
   flex-direction: column;
   gap: 10px;
+}
+
+.otf-include-confirmation {
+  margin: 0 0 2px;
+  color: #263746;
+  font-size: 13px;
+  font-weight: 500;
+  line-height: 1.35;
 }
 
 .otf-include-form .fm-field {
