784ed997e4ac5ba12e3cea7c9283ffaa6a2798f0
laucha <mlbuson@personal.com.ar>
2026-07-24 16:58:44 -0300

    fix(FMR-22520): arreglo de style scoped y position iframe

--- src/views/DetalleView.vue
+++ src/views/DetalleView.vue
@@ -1,16 +1,28 @@
 <template>
   <iframe
+    ref="iframeRef"
     :src="iframeSrc"
     :title="titulo"
     width="100%"
     frameborder="0"
     allowfullscreen
-    style="width: 100%; height: calc(100vh - 82px); min-height: 520px; display: block; border: 0;"
+    class="legacy-iframe"
+    @load="onIframeLoad"
   />
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import { useLegacyIframeLayout } from '@/composables/useLegacyIframeLayout'
+import { useLegacyIframeViewport } from '@/composables/useLegacyIframeViewport'
+
+const iframeRef = ref(null)
+const { onIframeLoad: applyLegacyLayout } = useLegacyIframeLayout(iframeRef)
+const { onIframeLoad: applyLegacyViewport } = useLegacyIframeViewport(iframeRef)
+const onIframeLoad = () => {
+  applyLegacyLayout()
+  applyLegacyViewport()
+}
 
 const iframeSrc = computed(() =>
   `${window.location.origin}/pc${sessionStorage.getItem('urlDetalle')}?nroActa=${sessionStorage.getItem('nroActa')}`
@@ -18,3 +30,11 @@ const iframeSrc = computed(() =>
 
 const titulo = `Detalle Acta - ${sessionStorage.getItem('nroActa') || ''}`
 </script>
+
+<style scoped>
+.legacy-iframe {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+} 
+</style>
