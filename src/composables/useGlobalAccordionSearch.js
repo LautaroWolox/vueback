2b1cb59195aca8d9d3b4d0037bab9319f6f570c6
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-25 19:00:57 -0300

    Inicializar abiertos los acordeones de Búsqueda de OTs

--- src/composables/useGlobalAccordionSearch.js
+++ src/composables/useGlobalAccordionSearch.js
@@ -1,12 +1,14 @@
 import { onBeforeUnmount, onMounted } from 'vue'
 import { installAccordionSearchBehavior } from '@/utils/accordionSearchBehavior'
 import { installMigratedAccordionSearchBehavior } from '@/utils/migratedAccordionSearchBehavior'
+import { installBuscadorOtsInitialPanels } from '@/utils/buscadorOtsInitialPanels'
 
 export function useGlobalAccordionSearch() {
   let removeBehaviors = []
 
   onMounted(() => {
     removeBehaviors = [
+      installBuscadorOtsInitialPanels(document),
       installAccordionSearchBehavior(document),
       installMigratedAccordionSearchBehavior(document)
     ]
