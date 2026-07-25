1dd93e8c62c771ae06e2c17a523035231962b28d
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-25 18:45:57 -0300

    Renderizar la nueva pantalla de Búsqueda de OTs

--- src/views/IframeView.vue
+++ src/views/IframeView.vue
@@ -1,5 +1,8 @@
 <template>
+  <BuscadorOts v-if="isBuscadorOts" />
+
   <iframe
+    v-else
     ref="iframeRef"
     :src="iframeSrc"
     :title="titulo"
@@ -14,6 +17,7 @@
 <script setup>
 import { computed, onUnmounted, ref, watchEffect } from 'vue'
 import router from '@/router'
+import BuscadorOts from '@/modules/buscadorOts/BuscadorOts.vue'
 import { useLegacyIframeLayout } from '@/composables/useLegacyIframeLayout'
 import { useLegacyIframeViewport } from '@/composables/useLegacyIframeViewport'
 
@@ -30,6 +34,7 @@ const onIframeLoad = () => {
   applyLegacyViewport()
 }
 const titulo = computed(() => props.titleParam || sessionStorage.getItem('titleParam') || '')
+const isBuscadorOts = computed(() => props.urlParam === '/busquedaOtsGcc.html')
 
 watchEffect(() => {
   sessionStorage.setItem('urlParam', props.urlParam)
