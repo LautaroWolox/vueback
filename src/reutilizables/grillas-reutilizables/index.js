dd8be7fe7eb35e05a4e961344143c4ab959d970e
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-26 22:48:28 -0300

    Crear entrada de grillas reutilizables

---
+++ src/reutilizables/grillas-reutilizables/index.js
@@ -0,0 +1,3 @@
+export { default as FmGridShell } from '../../components/shared/FmGridShell.vue'
+export { default as FmGridActions } from '../../components/shared/FmGridActions.vue'
+export { default as FmGridPaginator } from '../../components/shared/FmGridPaginator.vue'
