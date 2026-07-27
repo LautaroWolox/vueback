1e72c0e1f35713740cde5341579859eb8787ac5b
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-26 22:48:41 -0300

    Crear entrada de popups reutilizables

---
+++ src/reutilizables/popups-reutilizables/index.js
@@ -0,0 +1,4 @@
+export { default as FmDialog } from '../../components/shared/FmDialog.vue'
+export { default as FmAlertDialog } from '../../components/shared/FmAlertDialog.vue'
+export { default as FmConfirmDialog } from '../../components/shared/FmConfirmDialog.vue'
+export { default as FmGridDialog } from '../../components/shared/FmGridDialog.vue'
