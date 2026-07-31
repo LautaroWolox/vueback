3dbc309adf72b2cf1bb01e11d931b4ae93fad011
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-07-31 02:20:02 -0300

    Agregar columna Origen a Jobtype-Contrato

--- src/modules/parametrizaciones/jobtypeContrato/config/columns.js
+++ src/modules/parametrizaciones/jobtypeContrato/config/columns.js
@@ -1,15 +1,16 @@
 /** Columnas de la grilla principal de Jobtype-Contrato */
 export const JOBTYPE_CONTRATO_COLUMNS = [
-  { field: 'tareaContratoId',    header: '',                     width: '0',      hidden: true,  exportable: false, filter: false, sort: false },
-  { field: 'tareaId',            header: '',                     width: '0',      hidden: true,  exportable: false, filter: false, sort: false },
-  { field: 'contratoTipoId',     header: '',                     width: '0',      hidden: true,  exportable: false, filter: false, sort: false },
-  { field: 'tareaCodigo',        header: 'CODIGO_TAREA',         width: '14.28%', exportable: true, filter: true, sort: true },
-  { field: 'tareaNombre',        header: 'TAREA',                width: '14.28%', exportable: true, filter: true, sort: true },
-  { field: 'contratoNombre',     header: 'NOMBRE_CONTRATO',      width: '14.28%', exportable: true, filter: true, sort: true },
-  { field: 'legajoModificacion', header: 'USUARIO_MODIFICACION', width: '14.28%', exportable: true, filter: true, sort: true },
-  { field: 'fechaModificacion',  header: 'FECHA_MODIFICACION',   width: '14.28%', exportable: true, filter: true, sort: true },
-  { field: 'activo',             header: 'ACTIVO',               width: '14.28%', exportable: true, filter: true, sort: true },
-  { field: 'pais',               header: 'PAIS',                 width: '14.28%', exportable: true, filter: true, sort: true }
+  { field: 'tareaContratoId',    header: '',                     width: '0',     hidden: true,  exportable: false, filter: false, sort: false },
+  { field: 'tareaId',            header: '',                     width: '0',     hidden: true,  exportable: false, filter: false, sort: false },
+  { field: 'contratoTipoId',     header: '',                     width: '0',     hidden: true,  exportable: false, filter: false, sort: false },
+  { field: 'tareaCodigo',        header: 'CODIGO_TAREA',         width: '12.5%', exportable: true, filter: true, sort: true },
+  { field: 'tareaNombre',        header: 'TAREA',                width: '12.5%', exportable: true, filter: true, sort: true },
+  { field: 'origen',             header: 'ORIGEN',               width: '12.5%', exportable: true, filter: true, sort: true },
+  { field: 'contratoNombre',     header: 'NOMBRE_CONTRATO',      width: '12.5%', exportable: true, filter: true, sort: true },
+  { field: 'legajoModificacion', header: 'USUARIO_MODIFICACION', width: '12.5%', exportable: true, filter: true, sort: true },
+  { field: 'fechaModificacion',  header: 'FECHA_MODIFICACION',   width: '12.5%', exportable: true, filter: true, sort: true },
+  { field: 'activo',             header: 'ACTIVO',               width: '12.5%', exportable: true, filter: true, sort: true },
+  { field: 'pais',               header: 'PAIS',                 width: '12.5%', exportable: true, filter: true, sort: true }
 ]
 
 export const PAIS_OPTIONS = [
