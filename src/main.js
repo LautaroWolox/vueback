898488ad86f80e1665fd28f1237083c103f156b8
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-08-02 16:51:30 -0300

    Activar ajustes responsive de módulos en notebooks

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import './assets/css/jobtype-required-validation.css'
 import './assets/css/parametrizaciones-grid-selection.css'
 import './assets/css/fm-grid-empty-state.css'
 import './assets/css/fm-desktop-notebook-responsive.css'
+import './assets/css/fm-desktop-notebook-modules.css'
 
 import Lara from '@primeuix/themes/lara'
 import { definePreset } from '@primeuix/themes'
