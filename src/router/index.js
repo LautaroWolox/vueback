fd4fcf7f8f9c24dae846fd54da2d89befebf6aa6
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-08-03 15:22:31 -0300

    Enrutar Búsqueda de OTs directamente a su módulo

--- src/router/index.js
+++ src/router/index.js
@@ -309,11 +309,7 @@ const routes = [
         path: 'busquedaOtsGcc.html',
         name: 'BUOT',
         beforeEnter: allowed,
-        component: () => import('../views/IframeView.vue'),
-        props: {
-          urlParam: '/busquedaOtsGcc.html',
-          titleParam: 'Búsqueda de Ots'
-        }
+        component: () => import('../modules/buscadorOts/BuscadorOts.vue')
       }
     ]
   }
