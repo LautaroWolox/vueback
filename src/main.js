56786a5ddaff37902b9bf7a4c7871e21a4d4d586
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-08-02 22:23:02 -0300

    Activar separacion reducida debajo del menu

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import './assets/css/parametrizaciones-grid-selection.css'
 import './assets/css/fm-grid-empty-state.css'
 import './assets/css/fm-desktop-notebook-responsive.css'
 import './assets/css/fm-desktop-notebook-modules.css'
+import './assets/css/fm-menu-spacing.css'
 
 import Lara from '@primeuix/themes/lara'
 import { definePreset } from '@primeuix/themes'
