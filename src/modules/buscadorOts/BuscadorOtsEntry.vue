cd4320263b95e85aa6a828f2fe8f39ac4105a1f9
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-08-16 21:28:37 -0300

    feat: aislar entrada del Buscador de OTs

---
+++ src/modules/buscadorOts/BuscadorOtsEntry.vue
@@ -0,0 +1,15 @@
+<template>
+  <BuscadorOtsView v-if="useMigratedView" />
+  <IframeView
+    v-else
+    url-param="/busquedaOtsGcc.html"
+    title-param="Búsqueda de OTs"
+  />
+</template>
+
+<script setup>
+import BuscadorOtsView from './BuscadorOtsView.vue'
+import IframeView from '@/views/IframeView.vue'
+
+const useMigratedView = import.meta.env.DEV
+</script>
