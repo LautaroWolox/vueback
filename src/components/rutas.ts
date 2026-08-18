57660d527b498079f3b854efc58f437dd90ed25a
Laucha <111428491+LautaroWolox@users.noreply.github.com>
2026-08-18 00:36:13 -0300

    chore: quitar ABM Materiales del menú

--- src/components/rutas.ts
+++ src/components/rutas.ts
@@ -66,11 +66,6 @@ export const getRutas = (rutas:any) => {
                     visible: hasMenu('GEOT')
                 },
                 {
-                    label: 'ABM MATERIALES',
-                    command: () => { router.push({ name: 'ABMM' }); },
-                    visible: hasMenu('ABMM') || (import.meta.env.DEV && hasMenu('GEMA'))
-                },
-                {
                     label: 'Errores de Gestión',
                     command: () => { router.push({ name: 'ERGE' }); },
                     visible: hasMenu('ERGE')
